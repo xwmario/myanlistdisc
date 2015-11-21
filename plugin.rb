f7b58fec324c42c1162c4a96e14351559698f245
xwmario <alankrt@gmail.com>
2015-11-22 00:20:24 +0530

    Create plugin.rb

---
+++ plugin.rb
@@ -0,0 +1,9 @@
+# name: myanimelist
+# about: discourse customizations for myanimelist
+# version: 0.1
+# authors: xwmario code based Vikhyat
+
+plugins_dir = File.expand_path(File.dirname(__FILE__))
+
+### Onebox
+load File.expand_path('../onebox1.rb', __FILE__)
